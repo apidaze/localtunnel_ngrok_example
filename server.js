53b1ba562b99185e7f277215f9cca46ddfe3fdb1
phsultan <philippe.sultan@gmail.com>
2014-04-15 21:52:38 +0200

    Conform to latest express

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
  
 var app = express();
-app.configure(function(){
-	  app.use(express.static(__dirname + '/public'));
-	  app.use(app.router);
-});
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8333, function() {
     console.log("Listening on port 8333");
