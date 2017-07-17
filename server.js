6f9675662135daad2fb751a3575040bc1455adf0
Philippe Sultan <philippe.sultan@gmail.com>
2017-07-17 19:25:55 +0200

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
- 
-var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(8333, function() {
-    console.log("Listening on port 8333");
-});
